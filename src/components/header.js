26216f40303949eafeeb5eb61f5ae4883171da61
yenngan <58468798+yenngan@users.noreply.github.com>
2022-03-26 10:49:13 +0700

    update

--- src/components/header.js
+++ src/components/header.js
@@ -9,8 +9,8 @@ export default function MyHeader(){
           <div className="container">
             <div className="background-icon">
               <i className="fas fa-home" style={{color: "red"}}></i>
-              {/* <a className="navbar-brand" href="/" style={{margin:"0 0 0 10px"}}> MY MAP</a> */}
-              <NavLink  className={"navbar-brand" } to="/" style={{margin:"0 0 0 10px"}} exact="true">MY MAP</NavLink>
+              <a className="navbar-brand" href="/" style={{margin:"0 0 0 10px"}}> MY MAP</a>
+              {/* <NavLink  className={"navbar-brand" } to="/" style={{margin:"0 0 0 10px"}} exact="true">MY MAP</NavLink> */}
             </div>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               
@@ -24,11 +24,11 @@ export default function MyHeader(){
                     Map
                   </a>
                   <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <NavLink className={"dropdown-item"} to="/" exact="true" >Street</NavLink>
-                    {/* <a className="dropdown-item" href="/" exact="true">Street</a> */}
+                    {/* <NavLink className={"dropdown-item"} to="/" exact="true" >Street</NavLink> */}
+                    <a className="dropdown-item" href="/" exact="true">Street</a>
                     <div className="dropdown-divider"></div>
-                    <NavLink className={"dropdown-item"} to="/satellite" >Satellite</NavLink>
-                    {/* <a className="dropdown-item" href="satellite">Satellite</a> */}
+                    {/* <NavLink className={"dropdown-item"} to="/satellite" >Satellite</NavLink> */}
+                    <a className="dropdown-item" href="satellite">Satellite</a>
                   </div>
                 </li>
                 <li className="nav-item dropdown">
@@ -39,8 +39,8 @@ export default function MyHeader(){
                   <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                     <a className="dropdown-item" href="postProject">Post project</a>
                     <div className="dropdown-divider"></div>
-                    {/* <a className="dropdown-item" href="myProject">My project</a> */}
-                    <NavLink  className={"dropdown-item" } to="myProject" > My project</NavLink>
+                    <a className="dropdown-item" href="myProject">My project</a>
+                    {/* <NavLink  className={"dropdown-item" } to="myProject" > My project</NavLink> */}
                    
                     <a className="dropdown-item" href="#">My news</a>
                     <div className="dropdown-divider"></div>
